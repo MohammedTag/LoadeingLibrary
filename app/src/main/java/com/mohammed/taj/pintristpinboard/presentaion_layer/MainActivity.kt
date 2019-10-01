5a423499c54397143ad16f6bb6dba613f8138e9b
Mohammed tag <mohamed.taj@thed.io>
2019-10-01 23:06:24 +0200

    -- adding main activity to git

---
+++ app/src/main/java/com/mohammed/taj/pintristpinboard/presentaion_layer/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.mohammed.taj.pintristpinboard.presentaion_layer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohammed.taj.pintristpinboard.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
+
+
+
+
+
